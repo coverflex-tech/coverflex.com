f5da5f13e0ca064cd6bef661dd091aed509381d7
ronaldofs <ronaldo.freitas.sousa@gmail.com>
2019-10-22 18:11:47 +0100

    add basic 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default (props) => (
+  <Layout title="Page not found">
+    <div className="section">
+      <div className="container">
+        <p className="title is-size-3">Page not found</p>
+      </div>
+    </div>
+  </Layout>
+)
