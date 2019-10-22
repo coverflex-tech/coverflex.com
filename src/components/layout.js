4a30383f94c3c98a9775aa6589c3ac37f2db70e5
ronaldofs <ronaldo.freitas.sousa@gmail.com>
2019-10-22 18:01:14 +0100

    add favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,18 @@ import React from "react"
 import Nav from "../components/nav.js"
 import Footer from "../components/footer.js"
 import { Helmet } from "react-helmet"
+import favicon16 from "../images/favicon16.png"
+import favicon32 from "../images/favicon32.png"
 
 export default ({ title, style, children, navbarClassname }) => (
   <div style={style}>
-    <Helmet>
+    <Helmet
+      link={[
+        { rel: "icon", type: "image/png", sizes: "16x16", href: `${favicon16}` },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: `${favicon32}` },
+        { rel: "shortcut icon", type: "image/png", href: `${favicon32}` },
+      ]}
+    >
       <meta charSet="utf-8" />
       <title>Coverflex{title ? " | " + title : ""}</title>
       <link rel="canonical" href="http://coverflex.com" />
