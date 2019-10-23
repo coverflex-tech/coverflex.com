bde8fab49d1e0fad408ed04e0143f1be07b5d65f
ronaldofs <ronaldo.freitas.sousa@gmail.com>
2019-10-23 13:23:29 +0100

    update privacy policy page

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -2,25 +2,31 @@ import React from "react"
 import Layout from "../components/layout"
 import { injectIntl, FormattedMessage } from "react-intl"
 
-export default injectIntl((props) => (
-  <Layout title="Privacy Policy" navbarClassname="is-light">
-    <div className="section has-background-light">
-      <div className="container">
-        <h1 className="title is-size-3">
+const Content = () => (
+  <div className="content">
+    <p className="is-size-3">Scope</p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque eligendi ut modi consequuntur impedit assumenda nesciunt nobis porro ab sunt aperiam delectus esse ullam aut quia, numquam debitis. Provident, facilis?
+    </p>
+  </div>
+)
+
+export default injectIntl(() => (
+  <Layout title="Privacy Policy">
+    <div className="section is-medium">
+      <div className="container has-text-centered">
+        <h1 className="title is-size-1-desktop is-size-2-touch">
           <FormattedMessage id="pages.privacyPolicy.title"/>
         </h1>
+        <br/>
+        <p className="is-size-6">Last updated: 01/11/2019</p>
       </div>
     </div>
     <div className="section">
       <div className="container">
-        <div className="columns">
+        <div className="columns is-centered">
           <div className="column is-7">
-            <div className="content">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Mollitia aliquam recusandae ratione inventore quas,
-              voluptatibus minima nemo qui dicta, debitis dolores temporibus quia adipisci vero?
-              Sequi provident temporibus error repellat.
-            </div>
+            <Content />
           </div>
         </div>
       </div>
