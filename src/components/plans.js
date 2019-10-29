6446ad5329e3cddd53c7716bbaf7ff528e27b313
ronaldofs <ronaldo.freitas.sousa@gmail.com>
2019-10-29 00:28:29 +0000

    update plans

--- src/components/plans.js
+++ src/components/plans.js
@@ -1,19 +1,89 @@
 import React, { useState } from "react"
-import { navigate } from "gatsby"
-import { injectIntl, FormattedMessage } from "react-intl"
+import { injectIntl, Link, FormattedMessage } from "gatsby-plugin-intl"
+
+const PLANS = ["essential", "intermediate", "premium"]
+
+const Plan = injectIntl(({ id, highlighted, opened, onOpen, intl }) => (
+  <div className={`section is-fullheight${highlighted ? " has-background-grey-dark has-text-white" : " has-background-light"}`}>
+    <div className="has-text-centered">
+      <p className={`title is-size-3-desktop is-size-4-touch${highlighted ? " has-text-white" : ""}`}>
+        <FormattedMessage id={`components.plans.${id}.title`} />
+      </p>
+      <Price amount={intl.formatMessage({ id: `components.plans.${id}.price` })} />
+      <br/>
+      <div className="is-hidden-touch">
+        <Link
+          className={`button is-primary is-radiusless is-medium${highlighted ? "" : " is-outlined"}`}
+          to={`/get-started/?plan=${id}`}
+        >
+          <FormattedMessage id="components.plans.more"/>
+        </Link>
+        <br/>
+        <br/>
+        <hr className={highlighted && "is-inverted"}/>
+      </div>
+      {!opened && (
+        <div className="is-hidden-desktop has-text-centered">
+          <button
+            className={`button is-primary is-radiusless is-medium${highlighted ? "" : " is-outlined"}`}
+            onClick={() => onOpen(id)}
+          >
+            <FormattedMessage id="components.plans.show"/>
+          </button>
+        </div>
+      )}
+    </div>
+    <div className={!opened ? "is-hidden-mobile" : "animated fadeIn fast"}>
+      <hr className={"is-hidden-desktop" + (highlighted ? " is-inverted" : "")}/>
+      {intl.messages[`components.plans.${id}.base`] && (
+        <p className="has-text-primary has-text-weight-bold">
+          <br/>
+          <FormattedMessage id={`components.plans.${id}.base`}/>
+          <br/>
+        </p>
+      )}
+      <br />
+      <p dangerouslySetInnerHTML={{__html: intl.formatMessage({ id: `components.plans.${id}.coverages` })}}></p>
+      <br />
+      <br />
+      <p className="is-size-7 has-text-grey">
+        <FormattedMessage id="components.plans.addons"/>
+      </p>
+      <br />
+      <p dangerouslySetInnerHTML={{__html: intl.formatMessage({ id: `components.plans.${id}.addons` })}}></p>
+      <br />
+    </div>
+    {opened && (
+      <div className="is-hidden-desktop has-text-centered">
+        <Link
+          className={`button is-primary is-radiusless is-medium${highlighted ? "" : " is-outlined"}`}
+          to={`/get-started/?plan=${id}`}
+        >
+          <FormattedMessage id="components.plans.more"/>
+        </Link>
+      </div>
+    )}
+  </div>
+))
 
 const Price = ({ amount }) => (
   <div
     className="columns is-mobile is-vcentered is-variable is-1"
     style={{ marginBottom: "-.75rem" }}
   >
-    <div className="column is-narrow">
-      <p className="is-size-3 has-text-weight-bold has-text-primary">
-        <span className="is-size-4">&euro;</span>
+    <div className="column has-text-right">
+      <p className="is-size-7">
+        <FormattedMessage id="components.plans.from" />
+      </p>
+      <p className="is-size-7"><br/></p>
+    </div>
+    <div className="column">
+      <p className="is-size-2 has-text-weight-bold has-text-primary">
+        <span className="is-size-5">&euro;</span>
         <span>{amount}</span>
       </p>
     </div>
-    <div className="column">
+    <div className="column has-text-left">
       <p className="is-size-7">
         / <FormattedMessage id="components.plans.employee" />
       </p>
@@ -24,179 +94,52 @@ const Price = ({ amount }) => (
   </div>
 )
 
-const CTA = ({ label, to, onClick }) => (
-  <button
-    onClick={onClick ? onClick : () => navigate(to)}
-    className="button is-primary is-outlined is-medium"
-  >
-    {label}
-  </button>
-)
-
 export default injectIntl(({ intl }) => {
   const [openedPlans, setOpenedPlans] = useState({})
 
-  const accordionClass = "animated fadeIn fast"
-  const hiddenAccordionClass = "is-hidden-mobile"
+  const onOpen = id => setOpenedPlans({ ...openedPlans, [id]: true })
 
   return (
     <div className="plans container">
-      <div className="columns is-multiline is-variable is-3-touch is-0-desktop">
-        <div className="plans-item column is-3-desktop is-12-mobile is-6-tablet">
-          <div className="section is-fullheight has-background-light">
-            <p className="title is-size-4">
-              <FormattedMessage id="components.plans.standard.title" />
-            </p>
-            <p className="subtitle is-size-6">
-              <FormattedMessage id="components.plans.standard.subtitle" />
-            </p>
-            <hr />
-            <Price amount="7.99" />
-            <div className={!openedPlans.standard ? hiddenAccordionClass : accordionClass}>
-              <hr />
-              <p className="has-text-weight-bold">Workers Compensation</p>
-              <p>Mandatory</p>
-              <br />
-              <p className="has-text-weight-bold">Business Content</p>
-              <p>Up to 10,000&euro; / employee</p>
-              <br />
-              <p className="has-text-weight-bold">Baggage</p>
-              <br />
-              <p className="has-text-weight-bold">Mobile Equipment</p>
-              <p>Up to 1,000&euro; / employee</p>
-              <br />
-              <br />
-              <CTA
-                to="/get-started/?plan=standard"
-                label={intl.formatMessage({ id: "components.plans.more" })}
-              />
-            </div>
-            {!openedPlans.standard && (
-              <div className="is-hidden-desktop">
-                <br />
-                <CTA
-                  label={intl.formatMessage({ id: "components.plans.show" })}
-                  onClick={() =>
-                    setOpenedPlans({ ...openedPlans, standard: true })
-                  }
-                />
-              </div>
-            )}
-          </div>
+      <div className="columns is-multiline">
+        <div className="column is-4-desktop is-12-mobile is-4-tablet">
+          <Plan id="essential" opened={openedPlans.essential} onOpen={onOpen}/>
         </div>
-        <div className="plans-item is-selected column is-3-desktop is-12-mobile is-6-tablet has-text-white">
-          <div className="section is-fullheight has-background-grey-dark">
-            <p className="is-size-7">
-              <br />
-            </p>
-            <p className="title is-size-4 has-text-white">
-              <FormattedMessage id="components.plans.intermediate.title" />
-            </p>
-            <p className="subtitle is-size-6 has-text-white">
-              <FormattedMessage id="components.plans.intermediate.subtitle" />
-            </p>
-            <hr className="is-inverted" />
-            <Price amount="19.99" />
-            <div
-              className={!openedPlans.intermediate ? hiddenAccordionClass : accordionClass}
-            >
-              <hr className="is-inverted" />
-              <p className="has-text-weight-bold has-text-primary">
-                All Standard features
-              </p>
-              <hr className="is-inverted" />
-              <p className="has-text-weight-bold">Serious Diseases</p>
-              <br />
-              <p className="has-text-weight-bold">Basic Health Insurance</p>
-              <br />
-              <p className="has-text-weight-bold">Flight Delays</p>
-              <br />
-              <br />
-              <CTA
-                to="/get-started/?plan=intermediate"
-                label={intl.formatMessage({ id: "components.plans.more" })}
-              />
-            </div>
-            {!openedPlans.intermediate && (
-              <div className="is-hidden-desktop">
-                <br />
-                <CTA
-                  label={intl.formatMessage({ id: "components.plans.show" })}
-                  onClick={() =>
-                    setOpenedPlans({ ...openedPlans, intermediate: true })
-                  }
-                />
-              </div>
-            )}
-          </div>
+        <div className="is-selected column is-4-desktop is-12-mobile is-4-tablet">
+          <Plan highlighted id="intermediate" opened={openedPlans.intermediate} onOpen={onOpen}/>
         </div>
-        <div className="plans-item column is-3-desktop is-12-mobile is-6-tablet">
-          <div className="section is-fullheight has-background-light">
-            <p className="title is-size-4">
-              <FormattedMessage id="components.plans.premium.title" />
-            </p>
-            <p className="subtitle is-size-6">
-              <FormattedMessage id="components.plans.premium.subtitle" />
-            </p>
-            <hr />
-            <Price amount="39.99" />
-            <div className={!openedPlans.premium ? hiddenAccordionClass : accordionClass}>
-              <hr />
-              <p className="has-text-weight-bold has-text-primary">
-                All Intermediate features
-              </p>
-              <hr />
-              <p className="has-text-weight-bold">Full Health Insurance</p>
-              <br />
-              <p className="has-text-weight-bold">Personal Accidents</p>
-              <p>Global 24/7 Coverage</p>
-              <br />
-              <p className="has-text-weight-bold">Cyber Risks</p>
-              <br />
-              <p className="has-text-weight-bold">Life Insurance</p>
-              <br />
-              <br />
-              <CTA
-                to="/get-started/?plan=premium"
-                label={intl.formatMessage({ id: "components.plans.more" })}
-              />
-            </div>
-            {!openedPlans.premium && (
-              <div className="is-hidden-desktop">
-                <br />
-                <CTA
-                  label={intl.formatMessage({ id: "components.plans.show" })}
-                  onClick={() =>
-                    setOpenedPlans({ ...openedPlans, premium: true })
-                  }
-                />
-              </div>
-            )}
-          </div>
+        <div className="column is-4-desktop is-12-mobile is-4-tablet">
+          <Plan id="premium" opened={openedPlans.premium} onOpen={onOpen}/>
         </div>
-        <div className="plans-item column is-3-desktop is-12-mobile is-6-tablet">
-          <div className="section is-fullheight has-background-light">
-            <p className="title is-size-4">
+        <div className="column is-12">
+          <p className="has-text-right is-size-7 is-hidden-touch">
+            <FormattedMessage id="components.plans.disclaimer" />
+          </p>
+          <p className="has-text-centered is-size-7 is-hidden-desktop">
+            <FormattedMessage id="components.plans.disclaimer" />
+          </p>
+        </div>
+        <br/>
+        <div className="column is-12">
+          <div className="is-fullheight has-text-centered">
+            <div className="is-hidden-touch"><br/><br/></div>
+            <p className="is-size-2-desktop is-size-3-touch has-text-weight-bold">
               <FormattedMessage id="components.plans.enterprise.title" />
             </p>
-            <p className="subtitle is-size-6">
+            <br/>
+            <p className="is-size-5-desktop is-size-6-touch">
               <FormattedMessage id="components.plans.enterprise.subtitle" />
             </p>
-            <hr />
-            <div className="has-text-light-grey">
-              <FormattedMessage id="components.plans.enterprise.text" />
-            </div>
-            <br />
-            <CTA
+            <br/>
+            <Link
+              className="button is-primary is-radiusless is-medium"
               to="/get-started/?plan=enterprise"
-              label={intl.formatMessage({ id: "components.plans.contact" })}
-            />
+            >
+              <FormattedMessage id="components.plans.contact"/>
+            </Link>
           </div>
         </div>
       </div>
-      <p className="has-text-right is-size-7">
-        <FormattedMessage id="components.plans.disclaimer" />
-      </p>
     </div>
   )
 })
