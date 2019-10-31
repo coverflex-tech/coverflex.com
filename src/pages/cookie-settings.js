b983ee6510ae3b3f4ee5202682423cf105b8371b
ronaldofs <ronaldo.freitas.sousa@gmail.com>
2019-10-31 15:44:17 +0000

    update legal pages

--- src/pages/cookie-settings.js
+++ src/pages/cookie-settings.js
@@ -2,15 +2,13 @@ import React from "react"
 import Layout from "../components/layout"
 import { injectIntl, FormattedMessage } from "gatsby-plugin-intl"
 
-export default injectIntl(({ intl }) => (
-  <Layout title="Privacy Policy">
+export default injectIntl(() => (
+  <Layout title="Cookie Settings">
     <div className="section is-medium">
       <div className="container has-text-centered">
         <h1 className="title is-size-1-desktop is-size-2-touch">
-          <FormattedMessage id="pages.privacyPolicy.title" />
+          <FormattedMessage id="pages.cookieSettings.title" />
         </h1>
-        <br />
-        <p className="is-size-6">Last updated: 01/11/2019</p>
       </div>
     </div>
     <div className="section">
@@ -18,7 +16,6 @@ export default injectIntl(({ intl }) => (
         <div className="columns is-centered">
           <div className="column is-7">
             <div className="content">
-              <div dangerouslySetInnerHTML={{__html: intl.formatMessage({ id: "pages.privacyPolicy.content" })}}></div>
             </div>
           </div>
         </div>
