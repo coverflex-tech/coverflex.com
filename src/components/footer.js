31bd0f126ce6a098c0fdf83590cc122ca6e459e1
ronaldofs <ronaldo.freitas.sousa@gmail.com>
2019-11-04 12:53:30 +0000

    add faq links

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,6 +9,11 @@ const SOCIAL_LINKS = [
   { id: "linkedin", url: "" },
 ]
 
+const faqUrl = locale =>
+  locale === "pt" ?
+  "https://coverflex-support.zendesk.com/hc/pt-pt/sections/360003196659-FAQ" :
+  "https://coverflex-support.zendesk.com/hc/en-us/sections/360003196659-FAQ"
+
 const SocialMediaLinks = () => (
   <div className="is-size-5-desktop is-size-4-touch">
     {SOCIAL_LINKS.map(({ id, url }) => (
@@ -24,98 +29,100 @@ const SocialMediaLinks = () => (
   </div>
 )
 
-export default injectIntl(() => (
-  <div className="footer">
-    <div className="container">
-      <div>
-        <Logo />
+export default injectIntl(({ intl }) => {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div>
+          <Logo />
+        </div>
+        <div
+          className="columns"
+          style={{ lineHeight: "2.5", marginBottom: "4rem" }}
+        >
+          <div className="column is-4">
+            <h3 style={{ lineHeight: "1.5", marginTop: "0.5rem" }}>
+              <FormattedMessage id="components.footer.tagline" />
+            </h3>
+          </div>
+          <div className="column is-2">
+            <p className="has-text-weight-bold">
+              <FormattedMessage id="components.footer.company.title" />
+            </p>
+            <ul>
+              <li>
+                <Link className="has-text-grey-dark" to="/about">
+                  <FormattedMessage id="components.footer.company.about" />
+                </Link>
+              </li>
+              <li>
+                <a className="has-text-grey-dark" href={faqUrl(intl.locale)}>
+                  <FormattedMessage id="components.footer.company.faq" />
+                </a>
+              </li>
+              <li>
+                <Link className="has-text-grey-dark" to="/about#careers">
+                  <FormattedMessage id="components.footer.company.careers" />
+                  {" "}
+                  <sup className="has-text-primary">
+                    <FormattedMessage id="components.footer.company.hiring" />
+                  </sup>
+                </Link>
+              </li>
+              <li>
+                <a className="has-text-grey-dark" href="/blog">
+                  <FormattedMessage id="components.footer.company.blog" />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="column is-2">
+            <p className="has-text-weight-bold">
+              <FormattedMessage id="components.footer.legal.title" />
+            </p>
+            <ul>
+              <li>
+                <Link className="has-text-grey-dark" to="/privacy-policy">
+                  <FormattedMessage id="components.footer.legal.privacyPolicy" />
+                </Link>
+              </li>
+              <li>
+                <Link className="has-text-grey-dark" to="/cookie-settings">
+                  <FormattedMessage id="components.footer.legal.cookies" />
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="column is-2">
+            <p className="has-text-weight-bold">
+              <FormattedMessage id="components.footer.access.title" />
+            </p>
+            <ul>
+              <li>
+                <Link className="has-text-grey-dark" to="/signin">
+                  <FormattedMessage id="components.footer.access.member" />
+                </Link>
+              </li>
+              <li>
+                <Link className="has-text-grey-dark" to="/signin">
+                  <FormattedMessage id="components.footer.access.employer" />
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="column is-2">
+            <p className="has-text-weight-bold">
+              <FormattedMessage id="components.footer.contact" />
+            </p>
+            <p>hello@coverflex.com</p>
+            <SocialMediaLinks />
+          </div>
+        </div>
+        <hr />
+        <p className="is-size-7">
+          <FormattedMessage id="components.footer.copyright" />
+        </p>
       </div>
-      <div
-        className="columns"
-        style={{ lineHeight: "2.5", marginBottom: "4rem" }}
-      >
-        <div className="column is-4">
-          <h3 style={{ lineHeight: "1.5", marginTop: "0.5rem" }}>
-            <FormattedMessage id="components.footer.tagline" />
-          </h3>
-        </div>
-        <div className="column is-2">
-          <p className="has-text-weight-bold">
-            <FormattedMessage id="components.footer.company.title" />
-          </p>
-          <ul>
-            <li>
-              <Link className="has-text-grey-dark" to="/about">
-                <FormattedMessage id="components.footer.company.about" />
-              </Link>
-            </li>
-            <li>
-              <a className="has-text-grey-dark" href="/faq">
-                <FormattedMessage id="components.footer.company.faq" />
-              </a>
-            </li>
-            <li>
-              <Link className="has-text-grey-dark" to="/about#careers">
-                <FormattedMessage id="components.footer.company.careers" />
-                {" "}
-                <sup className="has-text-primary">
-                  <FormattedMessage id="components.footer.company.hiring" />
-                </sup>
-              </Link>
-            </li>
-            <li>
-              <a className="has-text-grey-dark" href="/blog">
-                <FormattedMessage id="components.footer.company.blog" />
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="column is-2">
-          <p className="has-text-weight-bold">
-            <FormattedMessage id="components.footer.legal.title" />
-          </p>
-          <ul>
-            <li>
-              <Link className="has-text-grey-dark" to="/privacy-policy">
-                <FormattedMessage id="components.footer.legal.privacyPolicy" />
-              </Link>
-            </li>
-            <li>
-              <Link className="has-text-grey-dark" to="/cookie-settings">
-                <FormattedMessage id="components.footer.legal.cookies" />
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="column is-2">
-          <p className="has-text-weight-bold">
-            <FormattedMessage id="components.footer.access.title" />
-          </p>
-          <ul>
-            <li>
-              <Link className="has-text-grey-dark" to="/signin">
-                <FormattedMessage id="components.footer.access.member" />
-              </Link>
-            </li>
-            <li>
-              <Link className="has-text-grey-dark" to="/signin">
-                <FormattedMessage id="components.footer.access.employer" />
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="column is-2">
-          <p className="has-text-weight-bold">
-            <FormattedMessage id="components.footer.contact" />
-          </p>
-          <p>hello@coverflex.com</p>
-          <SocialMediaLinks />
-        </div>
-      </div>
-      <hr />
-      <p className="is-size-7">
-        <FormattedMessage id="components.footer.copyright" />
-      </p>
     </div>
-  </div>
-))
+  )
+})
